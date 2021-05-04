f9ac1e10f6c095794c2b4f56c700b04204e9d92b
ccwangelaine <wangelaine@berkeley.edu>
2021-05-04 14:26:05 -0700

    some more logic in twilio code and changed db env port 5001

--- Twilio/receivesms.py
+++ Twilio/receivesms.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, redirect, session
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+import requests
 import uuid
+import json
 
 
 
 # account_sid = 'AC5f64d1e6b804bf55ed3125b84f93fa2b'
 # auth_token = 'cee5beca64a486de2b082834546dcd01'
 # client = Client(account_sid, auth_token)
-SECRET_KEY = 'a secret key1234567891011'
+SECRET_KEY = 'a secret key22'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -17,8 +19,9 @@ app.config.from_object(__name__)
 #ngrok http 5000
 
 ppl_dict = {}
+groups = []
 
-print (uuid.uuid1())
+#print (uuid.uuid1())
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
@@ -34,35 +37,44 @@ def sms_reply():
 
     if counter == 1:
         resp.message("Thank you for choosing Bear Connect! What is your name?")
+        ppl_dict["_id"] = str(uuid.uuid1())
+
     elif counter == 2:
         name = request.form['Body']
         ppl_dict['name'] = name.lower()
-        message = 'Hi {}, what is your major?'.format(name)
+        message = 'Hi {}, what is your major? (e.g. cs, ds, eecs)'.format(name)
         resp.message(message)
 
     elif counter == 3:
         major = request.form['Body']
-        resp.message("What class/Department would you like to find a study group for? (e.g. reply CS61B or INFO)")
-        ppl_dict['major'] = major.lower()
+        resp.message("What class/Department would you like to find a study group for? (e.g. reply CS61B or INFO253B)")
+        ppl_dict['major'] = major.lower().replace(" ", "")
 
     elif counter == 4:
         class_dept = request.form['Body']
-        ppl_dict['class'] = class_dept.lower()
+        ppl_dict['selectedClass'] = class_dept.lower()
         resp.message("What year are you? (e.g. Freshman, Sophomore, Junior, Senior, Graduate, Other)")
 
     elif counter == 5:
 
         year = request.form['Body']
-        ppl_dict['year'] = year.lower()
+        loweredYear = year.lower()
+        validYears = ['freshman', 'sophomore', 'junior', 'senior', 'graduate', 'other']
+        if loweredYear not in validYears:
+            counter = 4
+            session['counter'] = counter
+            resp.message('Input is invalid. What year are you? (e.g. Freshman, Sophomore, Junior, Senior, Graduate, Other)')
+            return str(resp)
+        ppl_dict['year'] = loweredYear
         message = 'Are you an early bird or night owl? Reply 1 for early bird, 2 for night owl'
         resp.message(message)
 
     elif counter == 6:
         study_time  = request.form['Body']
         if study_time == '1':
-            ppl_dict['studyTime'] = 'early_bird'
+            ppl_dict['studyTimes'] = 'early_bird'
         elif study_time == '2':
-            ppl_dict['studyTime'] = 'night_owl'
+            ppl_dict['studyTimes'] = 'night_owl'
         else:
             counter = 5
             session['counter'] = counter
@@ -99,32 +111,50 @@ def sms_reply():
             resp.message('Input is invalid. Which of the following best describe your study style? \n Reply 1 - Debugging Master  \n Reply 2 - Clubhouse Activists \n Reply 3 - Piazza Frontsitter \n Reply 4 - Visualization Guru')
             return str(resp)
 
+#relative logic
+        r = requests.post(url='http://localhost:5001/users/add', json=json.dumps(ppl_dict))
+        print(r.status_code)
+        print(r.reason)
+        resp.message('Awesome! Send any text to continue and then please wait a moment for results')
+
+#display group choices or return a new group
+    elif counter == 9:
+        r = requests.get(url='http://localhost:5001/groups/bestMatches', params=ppl_dict['_id'])
+        data = r.json()
+        groups = data['groups']
+        # if len(data['groups']) == 0:
+            # r = requests.post(url='http://localhost:5001/groups/startNew', json=json.dumps({"members": }))
+            # discordURL = r.json()['discordLink']
+            # resp.message('Ok, we created a new channel for ya! Here is the url: ' + discordURL + 'Feel free to invite others to discuss the topic on your mind!')
+
+        # TODO: format output strings displaying group choices
+        group1 = groups[0]
+        group2 = groups[1]
+        group3 = groups[2]
+
+#a group chosen
+    elif counter == 10:
+        groupChoice = request.form['Body']
+        if groupChoice == '1':
+            urlID = 'http://localhost:5001/groups/' + groups[0]['groupID']
+            r = requests.put(url=urlID, json = json.dumps({"userID":ppl_dict["id"]}))
+            discordURL = r.json()['discordLink']
+            resp.message("Done! You may now find your new group at " + discordURL )
+
+        elif groupChoice == '2':
+            urlID = 'http://localhost:5001/groups/' + groups[0]['groupID']
+            r = requests.put(url=urlID, json = json.dumps({"userID":ppl_dict["id"]}))
+            discordURL = r.json()['discordLink']
+            resp.message("Done! You may now find your new group at " + discordURL )
+
+        elif groupChoice == '3':
+            urlID = 'http://localhost:5001/groups/' + groups[0]['groupID']
+            r = requests.put(url=urlID, json = json.dumps({"userID":ppl_dict["id"]}))
+            discordURL = r.json()['discordLink']
+            resp.message("Done! You may now find your new group at " + discordURL )
+
     print(ppl_dict)
     return str(resp)
-
-    # ppl_dict['_id'] = uuid.uuid1()
-    #
-    # # Add a message
-    # if 'name' not in ppl_dict:
-    #     print('this is new code')
-    #     message = client.messages \
-    #                     .create(
-    #                          body="Thank you for choosing Bear Connect! What is your name?",
-    #                          from_='+15094245181',
-    #                          to='+17148227220'
-    #                      )
-    #
-    #     name = request.form['Body']
-    #     print(name)
-    #
-    #     ppl_dict['name'] = name
-    #
-    #
-    #     resp = MessagingResponse()
-    #     resp.message("Hello {}".format(name))
-    #     print(ppl_dict)
-    #
-    # return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
